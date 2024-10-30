a6e8d831c7dd30447a1b0bbbb2fb9234b2e7b62d
Luciano Fernandez <luchofseven@gmail.com>
2024-10-30 15:18:59 -0300

    feat: hero search query keywords

---
+++ src/lib/queryKeywords.ts
@@ -0,0 +1,13 @@
+import { generateInfoJobsKeywordsURL } from "./generate-infojobs-keywords-url";
+
+export const queryKeywords = (prefix: string) => {
+  const url = generateInfoJobsKeywordsURL({ prefix });
+
+  if (!url) return;
+
+  return fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+};
